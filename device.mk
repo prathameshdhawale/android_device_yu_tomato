7f252ee817dee2a3d2c0922f9d7244aa5991ae29
Prashant Maheshwari <prashantqwerty4@gmail.com>
2019-05-08 23:07:14 +0530

    It's Tomato bruh ;_;

--- device.mk
+++ device.mk
@@ -67,7 +67,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.mobee01a
+    android.hardware.light@2.0-service.tomato
 
 # Media
 PRODUCT_COPY_FILES += \
